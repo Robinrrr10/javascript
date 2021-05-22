09866537ebb1a4086dafe5b9d276b416469be59f
robinrrr10 <robinrrr10@gmail.com>
2021-05-22 21:14:04 +0530

    added variable

---
+++ latestTutorial/tutorial2variable.js
@@ -0,0 +1,25 @@
+num = 5  //this is old method to create variable
+console.log(num)
+
+let num2 = 6 //This is new method of creating the variable. So we can use this 
+//let is the keyword to create variable
+console.log(num2)
+
+let frdName = "Arupathi"
+console.log(frdName)
+
+let frd2Name = 'Aswin' //We can use double quote or single quote
+console.log(frd2Name)
+
+let action = "Aswin's mobile"
+console.log(action)
+
+let number = 3
+number = 8        //variable can change
+console.log(number)
+
+let given_number = 5
+console.log(given_number)
+
+let thatNumber = 34
+console.log(thatNumber)
