cb5f66cc7acc11926b6cec42b456b4b37f519122
robinrrr10 <robinrrr10@gmail.com>
2021-05-24 23:49:38 +0530

    added datatype

---
+++ latestTutorial/tutorial3constant.js
@@ -0,0 +1,9 @@
+let radius = 5
+const pi = 3.14 //const is the keyword used to declare constant variable
+
+//pi = 3.55  //We cannot change constant variable
+
+let area
+
+area = pi * radius * radius
+console.log(area)
---
+++ latestTutorial/tutorial4datatype.js
@@ -0,0 +1,24 @@
+let num = 8 //number
+let user = "ram" //string
+console.log(num)
+console.log(num+2)
+console.log(user)
+console.log(typeof num)
+console.log(typeof user)
+let decimal = 89.42
+console.log(typeof decimal)
+let lakh = 1_00_000
+console.log(lakh)
+console.log(typeof lakh)
+console.log(Number.MAX_VALUE)
+console.log(Number.MAX_VALUE*2)
+let large = 8769872978329732973972937n         //bigint number has n at end
+console.log(large)
+//console.log(large + 6n)
+console.log(large + 6n)
+let hexa = 0xf
+console.log(hexa)
+let nn = 78/0
+console.log(nn)
+let nmk = -78/0
+console.log(nmk)
