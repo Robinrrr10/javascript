975d473e3208563ba2675ca35c770dad92769285
robinrrr10 <robinrrr10@gmail.com>
2021-05-25 00:32:25 +0530

    added boolean, null, undefined

---
+++ latestTutorial/tutorial5datatype2.js
@@ -0,0 +1,29 @@
+let user = "ram"
+console.log(user)
+let nextname = "kumar"
+let nm = user + nextname
+console.log(nm)
+let nm2 = user + " " + nextname
+console.log(nm2)
+let nm3 = "ramnesh \"factory\""
+console.log(nm3)
+let nm4 = "hello\nEvery one"
+console.log(nm4)
+let nm5 = "hello\tEvery one"
+console.log(nm5)
+
+let bool1 = true
+console.log(bool1)
+let bool2 = 5 > 7
+console.log(bool2)
+
+let str = null
+console.log(str)
+console.log(typeof str)
+
+let str2
+console.log(str2)
+console.log(typeof str2)
+
+console.log( 2 / "hi")
+console.log( typeof (2 / "hi"))
