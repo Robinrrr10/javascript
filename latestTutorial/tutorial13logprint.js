6dca7b40b2fd9bbb0684926f50f944e6f3798f48
robinrrr10 <robinrrr10@gmail.com>
2021-05-30 22:02:19 +0530

    added priting techniq

---
+++ latestTutorial/tutorial13logprint.js
@@ -0,0 +1,8 @@
+let num1 = 4;
+let num2 = 7;
+let  result = num1 + num2 ;
+console.log("Addition of " + num1 + " and " + num2 + " is " + result); //first way to print
+console.log(`Addition of ${num1} and ${num2} is ${result}`); //second way to print. Here we used `
+console.log("Hi\nhow are you");//To go to next line i used \n here
+console.log(`hi
+how are you`);             //Here it will print as it is.
