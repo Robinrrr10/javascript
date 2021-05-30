470f04e41cc19c7bfeeb22b0a04129e42dc10d25
robinrrr10 <robinrrr10@gmail.com>
2021-05-30 22:19:35 +0530

    added while and do while

---
+++ latestTutorial/tutorial14whileanddowhile.js
@@ -0,0 +1,20 @@
+let num = 1;
+while(num <= 5)
+{
+    console.log("Hi", num);
+    num++;
+}
+
+let n = 10;
+while(n>=1)
+{
+    console.log("Hello", n)
+    n--;
+}
+
+let i = 8;
+do
+{
+    console.log("hey", i)
+    i++;
+}while(i < 5);
