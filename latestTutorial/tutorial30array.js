532c5f44b611f211373ce5a818d0097373226621
robinrrr10 <robinrrr10@gmail.com>
2021-09-10 12:33:43 +0530

    added array

---
+++ latestTutorial/tutorial30array.js
@@ -0,0 +1,16 @@
+let values = new Array(); //to create empty array
+console.log(values)
+
+let arr2 = []   //to create empty array
+console.log(arr2)
+
+let arr3 = [3, 8 , 3, 9]
+console.log(arr3)
+
+let arr4 = ["hi", "dai", "this"]
+console.log(arr4)
+arr4.push("just") //adding new values in array
+console.log(arr4)
+
+console.log("length of array is:" + arr4.length) //to  get length of the array
+console.log("value at index 2 is:" + arr4[2]) //to get particular value using index
