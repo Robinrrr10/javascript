94838925bd5303a33d2a40f0529dadd461f6962b
robinrrr10 <robinrrr10@gmail.com>
2021-09-11 13:38:29 +0530

    added array methods

---
+++ latestTutorial/tutorial32methodsofarray.js
@@ -0,0 +1,26 @@
+let arr = [12, 78, 9, 32, 11, 39, 71]
+console.log(arr)
+arr.push(24) //to add new element in last
+console.log(arr)
+arr.push(63, 85) //to add multiple new element
+console.log(arr)
+arr.pop() //to remove last element
+console.log(arr)
+console.log(arr.push(19)) //to add new element and it will return length of the array after adding new  element
+console.log(arr)
+console.log(arr.pop()) //to remove last element and it will return removed element
+console.log(arr)
+arr.shift() //to remove element at starting
+console.log(arr)
+arr.unshift(56) //to add element in starting
+console.log(arr)
+arr.splice(6) //to remove all element after 4th index
+console.log(arr)
+arr.splice(2,1) //to remove one element inbetween by giving index
+console.log(arr)
+arr.splice(3, 2) //to remove two element from index 3
+console.log(arr)
+arr.push(34, 1, 91)
+console.log(arr)
+arr.splice(3, 2, 67) //to remove two element from 3rd index and to insert 67 in between
+console.log(arr)
