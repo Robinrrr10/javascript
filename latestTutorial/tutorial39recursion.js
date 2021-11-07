7009a6acd6f05695bfcb627eeac5cf571567ea28
robinrrr10 <robinrrr10@gmail.com>
2021-11-07 23:20:43 +0530

    added recursion

---
+++ latestTutorial/tutorial39recursion.js
@@ -0,0 +1,9 @@
+let count = 1;
+function show() {
+console.log("hi:" + count)
+count++
+//if(count < 100) //we can controll using the condition also. uncomment and run
+ show() //Calling the same function
+}
+
+show()
